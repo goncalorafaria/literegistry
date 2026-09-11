5be078f77bf5d9657fa3db42b8e6ad1d2c0c6e61
Gonçalo Faria <gfaria@cs.washington.edu>
2026-09-11 17:13:13 -0500

    Bump literegistry and runtime image pins to 1.0.50

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ bm25_requirements = [
 
 setuptools.setup(
     name="literegistry",
-    version="1.0.49",
+    version="1.0.50",
     author="Goncalo Faria",
     author_email="gfaria@cs.washington.edu",
     description="Package for implementing service discovery in a really lite way.",
